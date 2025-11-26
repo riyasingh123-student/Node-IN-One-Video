15443ececc1d9f840b80cdf9960c0651b90c12fb
Riya Singh <riyasingh790322@gmail.com>
2025-11-26 19:07:38 +0530

    Add files via upload

---
+++ modulesecond.mjs
@@ -0,0 +1,11 @@
+ export function simple() {
+   console.log("Simple is complex")
+   return 45
+}
+
+export default function simple2() {
+   console.log("Simple2 is complex")
+}
+
+// module.exports = simple;
+
