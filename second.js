9660a0d7a5aeaac90ec8b7475a3fd530a8bef681
Riya Singh <riyasingh790322@gmail.com>
2025-11-26 19:09:39 +0530

    Add files via upload

---
+++ second.js
@@ -0,0 +1,12 @@
+// (function(exports, require, module, __filename, __dirname) {
+
+
+riya = {
+  name: "Riya",
+  favNum: 36,
+  devloper: true
+}
+console.log(exports, require, module, __filename, __dirname)
+module.exports = riya;
+
+// })
